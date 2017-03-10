65e0cf32dd8a8618779c13645f3eeeaea222ea34
Piffek <patrykpiwko123412@gmail.com>
2017-03-10 17:10:31 +0100

    pull data user who write subject

--- app/Subject.php
+++ app/Subject.php
@@ -3,10 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class Subject extends Model
 {
 	protected $fillable = [
 			'id', 'id_user', 'id_categories', 'body', 'subject'
 	];
+	
+	public function whoAddSubject($params)
+	{
+		return DB::table('Subjects')
+		->join('users', 'Subjects.id_user', '=', 'users.id')
+		->select('users.*')
+		->where([
+				['Subjects.id','=',$params->id],
+		])->get();
+	}
 }
