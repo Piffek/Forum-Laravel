dc217c8a257d9ddfa4b5a82f14d3ac883c53d124
Piffek <patrykpiwko123412@gmail.com>
2017-03-10 17:10:22 +0100

    pull data user who write posts

--- app/Posts.php
+++ app/Posts.php
@@ -1,14 +1,23 @@
 <?php
 
 namespace App;
+use DB;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Posts extends Model
 {
 	protected $fillable = [
-			'id', 'id_subject', 'id_user',
+			'id','text', 'id_subject', 'id_user',
 	];
 	
+	public function whoAddPosts($params)
+	{
+		return DB::table('Posts')
+		->join('users', 'Posts.id_user', '=', 'users.id')
+		->select('users.*', 'posts.*')
+		->where([['Posts.id_subject','=',$params->id]])
+		->get();
+	}
 
 }
