5426cc3b94c5314e97b51bbf024db4b768343e22
Piffek <patrykpiwko123412@gmail.com>
2017-03-14 16:56:19 +0100

    migration to php 7.1.1

--- database/seeds/user_role.php
+++ database/seeds/user_role.php
@@ -17,13 +17,13 @@ class user_role extends Seeder
 		$user->email = 'patryk@kwak.az.pl';
 		$user->password = bcrypt('promotion1');
 		$user->save();
-		$user->roles()->attach(10);
+		$user->roles()->attach(1);
 		
 		$user = new User();
 		$user->name = 'Patryk2';
 		$user->email = 'patryk2@kwak.az.pl';
 		$user->password = bcrypt('promotion2');
 		$user->save();
-		$user->roles()->attach(11);
+		$user->roles()->attach(2);
     }
 }
