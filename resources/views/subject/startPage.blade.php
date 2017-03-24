492305d7dffec67268f7a1b0bf1c3981820989a8
Piffek <patrykpiwko123412@gmail.com>
2017-03-24 20:42:38 +0100

    add session alert

--- resources/views/subject/startPage.blade.php
+++ resources/views/subject/startPage.blade.php
@@ -5,28 +5,36 @@
 
 	<div class="row">
 
-			<div class="col m6">
+			<div class="col m6" id="first">
 			<h1>Nowe tematy</h1>
 				<div class="card-panel col s12 m8 l6">
+				@if(count($subjects) === 0)
+						Obecnie nie ma nowych tematów
+				@else
 					 @foreach ($subjects as $subject)
 					   	<div class="card-panel grey lighten-5 z-depth-1">
 					   		<div class="row valign-wrapper">
 								<div class="col s10">
 									<span class="black-text">
-							   			Temat: <a href="{{ route('showOneSubject', $subject->id) }}">{{$subject->subject}}</a>
-						    					<p>Napisał: <b>{{$subject->name}}</b><br></p>
-						    					<p>{{$subject->created_at}}</p>
+											Temat: <a href="{{ route('showOneSubject', $subject->id) }}">{{$subject->subject}}</a>
+													<p>Napisał: <b>{{$subject->name}}</b><br></p>
+													<p>{{$subject->created_at}}</p>
 						    		</span>
 								</div>
 							</div>
 						</div>
 				    @endforeach
+						@endif
+
 				 </div>
 	    	</div>
 	    	
 			<div class="col m6">
 	    		<h1>Nowi użytkownicy</h1>
 	    			<div class="card panel col s12 m8 l6">    
+					@if(count($users) === 0)
+						Obecnie nie ma nowych użytkowników
+						@else
 				   		@foreach ($users as $user)
 				   			<div class="card-panel grey lighten-5 zdepth-1">
 				   				<div class="row valign-wrapper">
@@ -43,6 +51,7 @@
 			    			</div>
 		
 			    		@endforeach
+					@endif
 			    	</div>
 		    </div>
 		    </div>
